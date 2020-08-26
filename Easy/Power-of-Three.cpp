ef51c54fa14f1601a7240657c65496b1c81b0464
iayushvarshney <69698775+iayushvarshney@users.noreply.github.com>
2020-08-27 04:45:00 +0530

    Create Power-of-Three.cpp

---
+++ Easy/Power-of-Three.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        
+        if(n==0) return false;
+        
+        while(n!=1){
+            if(n%3==0) 
+                n/=3;
+            else
+                return false;
+        }
+        
+        if(n==1)
+            return true;
+        else
+            return false;
+        
+    }
+};
