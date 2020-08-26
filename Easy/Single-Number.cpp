642e256f03d0b66a7388f551843638f1627e5fad
iayushvarshney <69698775+iayushvarshney@users.noreply.github.com>
2020-08-26 11:10:05 +0530

    Create Single-Number.cpp

---
+++ Easy/Single-Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int temp=0;
+        
+        for(int i=0 ; i<nums.size(); i++){
+            temp^=nums[i];
+        }
+        
+        return temp;
+        
+    }
+};
