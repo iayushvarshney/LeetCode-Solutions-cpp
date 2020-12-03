634f23690692334556bfc359629f2a0d64c05aca
iayushvarshney <69698775+iayushvarshney@users.noreply.github.com>
2020-12-03 12:54:35 +0530

    Create peak-index-in-a-mountain-array.cpp

---
+++ Easy/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        
+        return find(arr.begin(), arr.end(), *max_element(arr.begin() , arr.end()))-arr.begin();;
+        
+    }
+};
