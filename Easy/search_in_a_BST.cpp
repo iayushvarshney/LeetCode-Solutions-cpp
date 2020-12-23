4be64f062ba5adf39a41dd3513399c736977fd96
iayushvarshney <69698775+iayushvarshney@users.noreply.github.com>
2020-12-24 00:59:41 +0530

    Create search_in_a_BST.cpp

---
+++ Easy/search_in_a_BST.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+TreeNode* searchBST(TreeNode* root, int val) {
+    if(root == NULL || root->val == val) return root;
+    else if(root->val > val) return searchBST(root->left, val);            
+    else return searchBST(root->right, val);            
+    }
+
+};
