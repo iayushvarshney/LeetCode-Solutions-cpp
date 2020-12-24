66316d7a04b1a7421898aeffdacb72e242ac7424
iayushvarshney <69698775+iayushvarshney@users.noreply.github.com>
2020-12-24 23:34:51 +0530

    Create richest-customer-wealth.cpp

---
+++ Easy/richest-customer-wealth.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        
+        int sum=0, max=0;
+        
+        for(int i=0 ; i<accounts.size() ; i++){
+            for(int j=0 ; j<accounts[0].size() ; j++){
+                sum+=accounts[i][j];
+            }
+            if(sum>max) max=sum;
+            sum=0;
+        }
+        
+        return max;
+    }
+};
