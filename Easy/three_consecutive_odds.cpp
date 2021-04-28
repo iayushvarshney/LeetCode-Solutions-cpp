b5a1b35fe36c18971cf8d3f12bff275ac228a9c7
iayushvarshney <69698775+iayushvarshney@users.noreply.github.com>
2021-04-28 14:29:41 +0530

    Create three_consecutive_odds.cpp

---
+++ Easy/three_consecutive_odds.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count=0;
+        
+        
+        for( int i: arr){
+            if(i%2!=0){
+                count++;
+            }
+            else
+                count=0;
+            
+            if(count==3)
+                return true;
+        }
+        
+        return false;
+        
+    }
+};
