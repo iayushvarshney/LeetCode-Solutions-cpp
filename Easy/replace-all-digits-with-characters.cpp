99b41146fa25c09a40966de221b1f5fa3b75e983
iayushvarshney <69698775+iayushvarshney@users.noreply.github.com>
2021-06-15 13:17:04 +0530

    Create replace-all-digits-with-characters.cpp

---
+++ Easy/replace-all-digits-with-characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string replaceDigits(string s) {
+        
+        int temp;
+        
+        for(int i=1 ; i<s.size() ; i+=2){
+           
+            s[i] += (s[i-1] - 48);
+            
+        }
+        
+        return s;
+        
+    }
+};
